945496e12c7674d8d952f6f94088c8ba3fdb1bc9
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2025-07-21 10:08:44 -0400

    Remove unused code and comments, re-organize and rename fn()s for better clarity and test initFullDoc() with console.logs to ensure it is functioning as intended

--- index.js
+++ index.js
@@ -4,21 +4,14 @@
 // run.test();
 
 
-// TODO: set event listener to body (??) to call hideAllMenus when when a click event takes place outside of btns (may just put it on content wrapper if putting it on body causes errors)
+// Query Selectors
 
-// query selectors
 const body = document.querySelector('body');
 const navMenu_allGroupDivs = document.querySelectorAll('.menu')
-// const btn_group1 = document.querySelector('.group-1 > button');
-// const btn_group2 = document.querySelector('.group-2 > button');
-// const btn_group3 = document.querySelector('.group-3 > button');
-
 const options_group1 = document.querySelectorAll('.group-1 li');
 const options_group2 = document.querySelectorAll('.group-2 li');
 const options_group3 = document.querySelectorAll('.group-3 li');
 
-// console.log(navMenu_allGroupDivs)
-// console.log(options_group1)
 
 // Menu fn()s
 
@@ -54,25 +47,20 @@ function deselectOption(e) {
     e.target.classList.remove('selected');
 }
 
-// event listeners
+function optionEvent(e) {
+    alert(`You have selected ${e.target.textContent}`)
+}
 
-// TODO: set options to only initialize when menu is open and to remove listeners when menu is closed
 
-// UPD TODO: Change back to original init method, as attaching the initOptionEvents to initDisplayEvents caused the options to be re-initialized every time a menu option was clicked. 
-// Also, when used in combo with display:none, the alert would be triggered immediately upon closing 
+// Init Events
 
-function initDisplayEvents(nodeList) {
+function initMenuEvents(nodeList) {
     for (const element of nodeList) {
-        element.addEventListener('click', (e) => {
-            toggleMenu(e, nodeList);
-            // initOptionEvents(e)
-        });
+        element.addEventListener('click', (e) =>     toggleMenu(e, nodeList));
     }
 }
 
 function initOptionEvents(optionGroup) {
-    // const optionGroup = e.target.parentNode.childNodes.item(3).children;
-    console.log(optionGroup)
     for (const option of optionGroup) {
         option.addEventListener('mouseenter', (e) => selectOption(e));
         option.addEventListener('mouseleave', (e) => deselectOption(e));
@@ -82,23 +70,15 @@ function initOptionEvents(optionGroup) {
 
 function initFullDoc(body, nodeList) {
     body.addEventListener('click', (e) => {
-        console.log(e.target)
-        console.log(e.target.closest('.menu'))
+        console.log(`Target is not a child of .menu: ${!e.target.closest('.menu')}`);
         if (!e.target.closest('.menu')) {
-            hideAllMenus(nodeList)
+            hideAllMenus(nodeList);
         }
     })
 }
 
 
-
-function optionEvent(e) {
-    const selection = e.target.textContent
-    console.log(selection)
-    alert(`You have selected ${e.target.textContent}`)
-}
-
-initDisplayEvents(navMenu_allGroupDivs);
+initMenuEvents(navMenu_allGroupDivs);
 initFullDoc(body, navMenu_allGroupDivs);
 initOptionEvents(options_group1);
 initOptionEvents(options_group2);
